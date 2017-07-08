392ae37e7e5fe23ffcc31f505e07dc5086aea033
Kevin Blicharski <kevin-blicharski@uiowa.edu>
2017-07-08 16:34:32 -0500

    Adds a new component to the page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,18 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent
   ],
   imports: [
     BrowserModule,
     FormsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent, ServerComponent]
 })
 export class AppModule { }
---
+++ src/app/server/server.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-server',
+  template: `
+    <h3>
+    Server Component
+    </h3>
+  `
+})
+export class ServerComponent {
+
+}
