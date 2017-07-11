5218386c4556b3d68fa13f3ad89b3580a4d580b5
Kevin Blicharski <kevin-blicharski@uiowa.edu>
2017-07-11 14:09:10 -0500

    (Directives) Servers are now dynamically added to the list using *ngFor

--- basics_tutorial/src/app/servers/servers.component.ts
+++ basics_tutorial/src/app/servers/servers.component.ts
@@ -9,7 +9,8 @@ export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'No server was created!';
   serverCreated = false;
-  serverName = 'TestServer';
+  serverName = '';
+  servers = [];
 
   constructor() {
     setTimeout(() => this.allowNewServer = true, 2000)
@@ -19,6 +20,7 @@ export class ServersComponent implements OnInit {
   }
 
   onCreateServer() {
+    this.servers.push(this.serverName);
     this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
     this.serverCreated = true;
   }
