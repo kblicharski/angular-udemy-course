9a41338283b19ed09d041d99501305ff61aad316
Kevin Blicharski <kevin-blicharski@uiowa.edu>
2017-07-11 09:07:40 -0500

    Adds event binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServersComponent implements OnInit {
   allowNewServer = false;
+  serverCreationStatus = 'No server was created!';
 
   constructor() {
     setTimeout(() => this.allowNewServer = true, 2000)
@@ -15,4 +16,7 @@ export class ServersComponent implements OnInit {
   ngOnInit() {
   }
 
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created!';
+  }
 }
