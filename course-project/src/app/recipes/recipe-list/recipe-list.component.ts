033cca599dadfa7ba1cf19b3ce78ebf20c7700fc
Kevin Blicharski <kevin-blicharski@uiowa.edu>
2017-07-13 10:39:10 -0500

    (Project) Adds the skeleton for the recipe list

--- course-project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ course-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,12 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
+  recipes: Recipe[] = [
+    new Recipe('Microwaved Potato', 'Just microwave the potato', 'https://pixabay.com/p-448613/?no_redirect')
+  ];
 
   constructor() { }
 
