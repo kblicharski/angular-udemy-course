b9de0f602257a94ac1fe160f33afc05e0ea59891
Kevin Blicharski <kevin-blicharski@uiowa.edu>
2017-07-13 11:02:27 -0500

    (Project) Starts implementing the shopping list by adding a new model -- creates a shared directory for shared models

--- course-project/src/app/shopping-list/shopping-list.component.ts
+++ course-project/src/app/shopping-list/shopping-list.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Ingredient } from '../shared/ingredient.model';
+
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+  ingredients: Ingredient[] = [];
 
   constructor() { }
 
