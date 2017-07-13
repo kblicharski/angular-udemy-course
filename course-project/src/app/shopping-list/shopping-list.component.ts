94fc9e802c544662869b9f55d6fc8c4e1679424d
Kevin Blicharski <kevin-blicharski@uiowa.edu>
2017-07-13 11:04:55 -0500

    (Project) Displays shopping list items

--- course-project/src/app/shopping-list/shopping-list.component.ts
+++ course-project/src/app/shopping-list/shopping-list.component.ts
@@ -8,7 +8,10 @@ import { Ingredient } from '../shared/ingredient.model';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-  ingredients: Ingredient[] = [];
+  ingredients: Ingredient[] = [
+    new Ingredient('Potatoes', 5),
+    new Ingredient('Ramen', 10)
+  ];
 
   constructor() { }
 
