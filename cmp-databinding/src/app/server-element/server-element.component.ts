e4e355c7e0d36017b02e81d1e8cddbaf68e6fb3b
Kevin Blicharski <kevin-blicharski@uiowa.edu>
2017-07-14 12:45:46 -0500

    Adds the other form of component communication -- Output()

--- cmp-databinding/src/app/server-element/server-element.component.ts
+++ cmp-databinding/src/app/server-element/server-element.component.ts
@@ -1,12 +1,13 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
-  styleUrls: ['./server-element.component.css']
+  styleUrls: ['./server-element.component.css'],
+  encapsulation: ViewEncapsulation.Emulated
 })
 export class ServerElementComponent implements OnInit {
-  @Input() element: {type: string, name: string, content: string};
+  @Input('serverElement') element: {type: string, name: string, content: string};
 
   constructor() { }
 
