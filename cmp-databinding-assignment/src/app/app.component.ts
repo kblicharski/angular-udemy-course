6a3ed6768e51ace264484154ec58dfd51e84f1c7
Kevin Blicharski <kevin-blicharski@uiowa.edu>
2017-07-15 18:49:39 -0500

    (79) Finishes the components and data binding assignment -- pretty proud of this actually

--- cmp-databinding-assignment/src/app/app.component.ts
+++ cmp-databinding-assignment/src/app/app.component.ts
@@ -9,6 +9,7 @@ export class AppComponent {
   numberComponents: {'type': string, 'num': number}[] = [];
 
   createComponent(input: number) {
+    console.log(input);
     if(input % 2 === 0) {
       this.numberComponents.push({
         type: 'even',
