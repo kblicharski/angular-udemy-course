92cc8369f1cb6b9939d4a84deb8b8921d2f71319
Kevin Blicharski <kevin-blicharski@uiowa.edu>
2017-07-18 11:41:34 -0500

    (107) Accomplishes the first task of the assignment on my own

--- services-assignment/src/app/app.component.ts
+++ services-assignment/src/app/app.component.ts
@@ -1,21 +1,12 @@
 import { Component } from '@angular/core';
 
+import { UsersService } from './services/users.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [UsersService]
 })
 export class AppComponent {
-  activeUsers = ['Max', 'Anna'];
-  inactiveUsers = ['Chris', 'Manu'];
-
-  onSetToInactive(id: number) {
-    this.inactiveUsers.push(this.activeUsers[id]);
-    this.activeUsers.splice(id, 1);
-  }
-
-  onSetToActive(id: number) {
-    this.activeUsers.push(this.inactiveUsers[id]);
-    this.inactiveUsers.splice(id, 1);
-  }
 }
