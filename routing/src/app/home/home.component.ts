8c54db8b149fb50e4d8c820a48b9ff508e6f0cd5
Kevin Blicharski <kevin-blicharski@uiowa.edu>
2017-07-20 19:56:10 -0500

    (129) Adds a way to include query parameters and fragments to URLs, both in the template and programmaticaly

--- routing/src/app/home/home.component.ts
+++ routing/src/app/home/home.component.ts
@@ -13,9 +13,10 @@ export class HomeComponent implements OnInit {
   ngOnInit() {
   }
 
-  onLoadServers() {
+  onLoadServer(id: number) {
     // some complex calculations
     // now we navigate away
-    this.router.navigate(['/servers']);
+    this.router.navigate(['/servers', id, 'edit'],
+                         {queryParams: {allowEdit: '1'}, fragment: 'loading'});
   }
 }
