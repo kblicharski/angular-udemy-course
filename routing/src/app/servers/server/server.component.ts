2daa1779defd9bfe068cd7662a08ec2f1c06864f
Kevin Blicharski <kevin-blicharski@uiowa.edu>
2017-07-26 15:23:49 -0500

    (126) Implements the server component (important note -- type coercion is sometimes necessary)

--- routing/src/app/servers/server/server.component.ts
+++ routing/src/app/servers/server/server.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs/Subscription';
 
 import { ServersService } from '../servers.service';
 
@@ -10,14 +11,25 @@ import { ServersService } from '../servers.service';
     styleUrls: ['./server.component.css']
   }
 )
-export class ServerComponent implements OnInit {
+export class ServerComponent implements OnInit, OnDestroy {
   server: { id: number, name: string, status: string };
+  serverSubscription: Subscription;
 
   constructor(private serversService: ServersService,
               private route: ActivatedRoute) { }
 
   ngOnInit() {
-    const id = this.route.snapshot.params['id'];
+    const id = +this.route.snapshot.params['id'];
     this.server = this.serversService.getServer(id);
+    this.serverSubscription = this.route.params
+      .subscribe(
+        (params: Params) => {
+          this.server = this.serversService.getServer(+params['id']);
+        }
+      );
+  }
+
+  ngOnDestroy() {
+    this.serverSubscription.unsubscribe();
   }
 }
