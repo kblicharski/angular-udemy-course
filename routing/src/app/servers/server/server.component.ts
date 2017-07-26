f01c4ed3e7d9d01a70a3c9647b0a62a1095d41d5
Kevin Blicharski <kevin-blicharski@uiowa.edu>
2017-07-26 15:05:34 -0500

    (126) Adds my attempt to pass the server ID into the server component

--- routing/src/app/servers/server/server.component.ts
+++ routing/src/app/servers/server/server.component.ts
@@ -1,19 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 import { ServersService } from '../servers.service';
 
-@Component({
-  selector: 'app-server',
-  templateUrl: './server.component.html',
-  styleUrls: ['./server.component.css']
-})
+@Component(
+  {
+    selector: 'app-server',
+    templateUrl: './server.component.html',
+    styleUrls: ['./server.component.css']
+  }
+)
 export class ServerComponent implements OnInit {
-  server: {id: number, name: string, status: string};
+  server: { id: number, name: string, status: string };
 
-  constructor(private serversService: ServersService) { }
+  constructor(private serversService: ServersService,
+              private route: ActivatedRoute) { }
 
   ngOnInit() {
-    this.server = this.serversService.getServer(1);
+    const id = this.route.snapshot.params['id'];
+    this.server = this.serversService.getServer(id);
   }
-
 }
