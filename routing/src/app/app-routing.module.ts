8122b259d2b23a60113b27ac072db7b59d544403
Kevin Blicharski <kevin-blicharski@uiowa.edu>
2017-07-27 11:50:21 -0500

    (133) Splits the routes into their own module

--- routing/src/app/app-routing.module.ts
+++ routing/src/app/app-routing.module.ts
@@ -1,10 +1,32 @@
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
-@NgModule(
-  {
+import { HomeComponent } from './home/home.component';
+import { UsersComponent } from 'app/users/users.component';
+import { ServersComponent } from 'app/servers/servers.component';
+import { EditServerComponent } from 'app/servers/edit-server/edit-server.component';
+import { ServerComponent } from 'app/servers/server/server.component';
+import { UserComponent } from './users/user/user.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-  }
-)
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'users', component: UsersComponent, children: [
+    { path: ':id/:name', component: UserComponent }
+  ] },
+  { path: 'servers', component: ServersComponent, children: [
+    { path: ':id', component: ServerComponent },
+    { path: ':id/edit', component: EditServerComponent }
+  ] },
+  { path: '**', component: PageNotFoundComponent }
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(appRoutes)
+  ],
+  exports: [RouterModule]
+})
 export class AppRoutingModule {
 
 }
