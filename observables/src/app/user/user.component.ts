75a5a7b2a8ffcbdd4e3065fd7fd4c156752704dd
Kevin Blicharski <kevin-blicharski@uiowa.edu>
2017-07-31 11:15:41 -0500

    (163) Demonstrates how cross-component communication can be done using Subjects

--- observables/src/app/user/user.component.ts
+++ observables/src/app/user/user.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 
+import { UsersService } from 'app/user/users.service';
+
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
@@ -9,15 +11,20 @@ import { ActivatedRoute, Params } from '@angular/router';
 export class UserComponent implements OnInit {
   id: number;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute,
+              private usersService: UsersService) { }
 
   ngOnInit() {
     this.route.params
       .subscribe(
         (params: Params) => {
           this.id = +params['id'];
-        },
+        }
       );
+  }
+
+  onActivate() {
+    this.usersService.userActivated.next(this.id);
   }
 
 }
