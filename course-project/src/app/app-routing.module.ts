3c739bab053b5f64d0c15c909f500024e1a84564
Kevin Blicharski <kevin-blicharski@uiowa.edu>
2017-07-31 09:04:35 -0500

    (143) Adds a new component skeleton and hooks up the routerLinks

--- course-project/src/app/app-routing.module.ts
+++ course-project/src/app/app-routing.module.ts
@@ -1,28 +1,27 @@
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { RecipesComponent } from './recipes/recipes.component';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { RecipeEditComponent } from 'app/recipes/recipe-edit/recipe-edit.component';
 
-const appRoutes = [
+const appRoutes: Routes = [
+  { path: '', redirectTo: '/recipes', pathMatch: 'full' },
   { path: 'shopping',
     component: ShoppingListComponent },
   { path: 'recipes',
-    component: RecipesComponent,
-    children: [
-      { path: 'edit', component: RecipeEditComponent },
-      { path: 'detail', component: RecipeDetailComponent }
-    ]
+    component: RecipesComponent
+    // children: [
+    //   { path: 'edit', component: RecipeEditComponent },
+    //   { path: 'detail', component: RecipeDetailComponent }
+    // ]
   },
-  { path: '**', redirectTo: 'recipes'}
+  { path: '**', redirectTo: '/recipes'}
 ];
 
 @NgModule({
-  imports: [
-    RouterModule.forRoot(appRoutes)
-  ],
+  imports: [RouterModule.forRoot(appRoutes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
