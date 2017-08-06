d7944eab507bbfca23d94e158ef654d5c5faf044
Kevin Blicharski <kevin-blicharski@uiowa.edu>
2017-08-06 15:35:57 -0500

    (Assignment 7) Creates the reactive form skeleton

--- forms-reactive-assignment/src/app/app.component.ts
+++ forms-reactive-assignment/src/app/app.component.ts
@@ -1,9 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  projectForm: FormGroup;
+  forbiddenNames = ['Test', 'test'];
+
+  ngOnInit() {
+    this.projectForm = new FormGroup({
+      'projectName': new FormControl(null, Validators.required),
+      'email': new FormControl(null, [Validators.required, Validators.email]),
+      'status': new FormControl('active')
+    });
+  }
+
+  onSubmit() {
+    console.log(this.projectForm);
+  }
+
+  nameValidator() {
+
+  }
 }
