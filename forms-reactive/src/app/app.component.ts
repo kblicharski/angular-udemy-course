706084cadc57ee7ebe55faf0109216628c9fd557
Kevin Blicharski <kevin-blicharski@uiowa.edu>
2017-08-06 12:25:54 -0500

    (198) Shows how valueChanges and statusChanges can be used to closely monitor form changes

--- forms-reactive/src/app/app.component.ts
+++ forms-reactive/src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
 
+import { Observable } from 'rxjs/Observable';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -14,12 +16,17 @@ export class AppComponent implements OnInit {
   ngOnInit() {
     this.signUpForm = new FormGroup({
       'userData': new FormGroup({
+        // we need to `bind(this)` because in forbiddenNames we use `this`
         'username': new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),
-        'email': new FormControl(null, [Validators.required, Validators.email])
+        'email': new FormControl(null, [Validators.required, Validators.email], this.forbiddenEmails)
       }),
       'gender': new FormControl('male'),
       'hobbies': new FormArray([])
     });
+    this.signUpForm.valueChanges
+      .subscribe(value => console.log(value));
+    this.signUpForm.statusChanges
+      .subscribe(status => console.log(status));
   }
 
   onSubmit() {
@@ -28,15 +35,30 @@ export class AppComponent implements OnInit {
 
   onAddHobby() {
     const control = new FormControl(null, Validators.required);
+    // this must be casted or else you get an error
     (<FormArray>this.signUpForm.get('hobbies')).push(control);
   }
 
-  // {nameIsForbidden: true}
+  // a regular validator
   forbiddenNames(control: FormControl): {[s: string]: boolean} {
     if (this.forbiddenUsernames.indexOf(control.value) !== -1) {
       return {'nameIsForbidden': true};
     }
     // this is important -- if validation is successful you must pass nothing or null
     return null;
+  }
+
+  // example of an asynchronous validator
+  forbiddenEmails(control: FormControl): Promise<any> | Observable<any> {
+     return new Promise<any>(
+      (resolve, reject) => {
+        setTimeout(() => {
+          if (control.value === 'test@test.com') {
+            resolve({'emailIsForbidden': true});
+          } else {
+            resolve(null);
+          }
+        }, 1500);
+    });
   }
 }
