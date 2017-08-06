f65c48b5448f3f21aecffd26129ba2066acaa472
Kevin Blicharski <kevin-blicharski@uiowa.edu>
2017-08-06 15:59:45 -0500

    (Assignment 7) Changes the validator into an asynchronous one

--- forms-reactive-assignment/src/app/app.component.ts
+++ forms-reactive-assignment/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-root',
@@ -14,7 +15,7 @@ export class AppComponent implements OnInit {
 
   ngOnInit() {
     this.projectForm = new FormGroup({
-      'projectName': new FormControl(null, [Validators.required, this.nameValidator.bind(this)]),
+      'projectName': new FormControl(null, Validators.required, this.nameValidator.bind(this)),
       'email': new FormControl(null, [Validators.required, Validators.email]),
       'status': new FormControl(this.defaultProjectStatus)
     });
@@ -24,10 +25,22 @@ export class AppComponent implements OnInit {
     console.log(this.projectForm);
   }
 
-  nameValidator(control: FormControl): {[s: string]: boolean} {
-    if (this.forbiddenNames.includes(control.value)) {
-      return {'forbiddenName': true};
-    }
-    return null;
+  // nameValidator(control: FormControl): {[s: string]: boolean} {
+  //   if (this.forbiddenNames.includes(control.value)) {
+  //     return {'forbiddenName': true};
+  //   }
+  //   return null;
+  // }
+
+  nameValidator(control: FormControl): Promise<any> | Observable<any> {
+    return new Promise<any>((resolve, reject) => {
+      setTimeout(() => {
+        if (this.forbiddenNames.includes(control.value)) {
+          resolve({'forbiddenName': true});
+        } else {
+          resolve(null);
+        }
+      }, 1500)
+    });
   }
 }
