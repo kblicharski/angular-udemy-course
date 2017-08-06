19687e6bd2c1a23c599f051353d2404ca79303fd
Kevin Blicharski <kevin-blicharski@uiowa.edu>
2017-08-05 19:17:16 -0500

    (189) Defines the reactive form in the .ts file and binds it to the formGroup directive in the template

--- forms-reactive/src/app/app.component.ts
+++ forms-reactive/src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   genders = ['male', 'female'];
+  signUpForm: FormGroup;
+
+  ngOnInit() {
+    this.signUpForm = new FormGroup({
+      'username': new FormControl(null),
+      'email': new FormControl(null),
+      'gender': new FormControl('male')
+    });
+  }
 }
