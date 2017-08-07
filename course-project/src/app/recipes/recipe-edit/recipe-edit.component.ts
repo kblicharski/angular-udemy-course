1b21235c83255ebc5ebe30ee4afc626a8ba8efad
Kevin Blicharski <kevin-blicharski@uiowa.edu>
2017-08-06 19:30:57 -0500

    (209) Creates HTML skeleton for the reactive form approach

--- course-project/src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ course-project/src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -16,7 +16,7 @@ export class RecipeEditComponent implements OnInit {
     this.route.params.subscribe(
       (params: Params) => {
         this.id = +params['id'];
-        this.editMode = params['id'] != null;
+        this.editMode = params['id'] !== null;
       }
     )
   }
