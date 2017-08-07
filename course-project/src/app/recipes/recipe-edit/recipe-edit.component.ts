f103d592ecb707ee9b29ec18287e0ff38f17d77a
Kevin Blicharski <kevin-blicharski@uiowa.edu>
2017-08-07 07:38:55 -0500

    (210) Changes my code according to Max's implementation

--- course-project/src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ course-project/src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import { FormGroup, FormControl, FormArray, Validators } from '@angular/forms';
 
+import { RecipeService } from 'app/recipes/recipe.service';
+
 @Component({
   selector: 'app-recipe-edit',
   templateUrl: './recipe-edit.component.html',
@@ -12,24 +14,39 @@ export class RecipeEditComponent implements OnInit {
   editMode = false;
   recipeForm: FormGroup;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute,
+              private recipeService: RecipeService) { }
 
   ngOnInit() {
     this.route.params.subscribe(
       (params: Params) => {
         this.id = +params['id'];
         this.editMode = params['id'] !== null;
+        this.initForm();
       }
     );
+  }
+
+  private initForm() {
+    let recipeName = '';
+    let recipeImagePath = '';
+    let recipeDescription = '';
+
+    if (this.editMode) {
+      const recipe = this.recipeService.getRecipe(this.id);
+      recipeName = recipe.name;
+      recipeImagePath = recipe.imagePath;
+      recipeDescription = recipe.description;
+    }
 
     this.recipeForm = new FormGroup({
-      'name': new FormControl(null, Validators.required),
-      'description': new FormControl(null, Validators.required),
-      'imagePath': new FormControl(null)
-    //   'recipeIngredients': new FormArray([], Validators.required)
-    })
+      'name': new FormControl(recipeName, Validators.required),
+      'description': new FormControl(recipeImagePath, Validators.required),
+      'imagePath': new FormControl(recipeDescription)
+      // 'recipeIngredients': new FormArray([], Validators.required)
+    });
   }
-  //
+
   // addIngredient() {
   //   const control = new FormControl(null, Validators.required);
   //   // this must be casted or else you get an error
