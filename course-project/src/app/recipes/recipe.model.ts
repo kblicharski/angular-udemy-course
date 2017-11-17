98b1054c62765e1622d36429eb8508995b7fbfe1
Kevin Blicharski <kevin-blicharski@uiowa.edu>
2017-11-17 12:34:42 -0700

    Reset project and create a blank slate.
    The course has been updated to Angular 5
    and I feel a little rusty with my skills.
    I think this is a good time to work through
    the course again, but this time much faster,
    with better commit messages, and less of them.
    I feel more confident as a developer and I
    believe I will gain more from this experience
    this time.

--- course-project/src/app/recipes/recipe.model.ts
+++
@@ -1,8 +0,0 @@
-import { Ingredient } from '../shared/models/ingredient.model';
-
-export class Recipe {
-  constructor(public name: string,
-              public description: string,
-              public imagePath: string,
-              public ingredients: Ingredient[]) {}
-}
--- observables/src/app/user/users.service.ts
+++
@@ -1,5 +0,0 @@
-import { Subject } from 'rxjs/Subject';
-
-export class UsersService {
-  userActivated = new Subject();
-}
