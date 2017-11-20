1af6aabf7e12a26068434580560e5a28d4ada894
Kevin Blicharski <kevin-blicharski@uiowa.edu>
2017-11-19 21:08:57 -0600

    (Project) Individual list items work.

--- course-project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ course-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('potato', 'a potato', 'https://cdn.shopify.com/s/files/1/1017/2183/t/2/assets/live-preview-potato.png?17662155388061927543')
+  ];
 
   constructor() { }
 
