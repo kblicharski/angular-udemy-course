37908188c5b92b20803bebce13a66e430307dd99
Kevin Blicharski <kevin-blicharski@uiowa.edu>
2017-11-19 20:51:21 -0600

    (Project) Finish header structuring.

--- course-project/src/app/app.module.ts
+++ course-project/src/app/app.module.ts
@@ -10,7 +10,6 @@ import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { ShoppingListEditComponent } from './shopping-list/shopping-list-edit/shopping-list-edit.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 
 @NgModule({
@@ -22,7 +21,6 @@ import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-ed
     RecipeDetailComponent,
     RecipeItemComponent,
     ShoppingListComponent,
-    ShoppingListEditComponent,
     ShoppingEditComponent
   ],
   imports: [
