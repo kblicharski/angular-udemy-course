6f6b894354a7094ad807c3c4a819d289d55719b7
Kevin Blicharski <kevin-blicharski@uiowa.edu>
2017-11-19 20:22:26 -0600

    (Basics) Finish Assignment 1.

--- assignments/basics-assignment-1/src/app/app.module.ts
+++ assignments/basics-assignment-1/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WarningAlertComponent,
+    SuccessAlertComponent
   ],
   imports: [
     BrowserModule,
