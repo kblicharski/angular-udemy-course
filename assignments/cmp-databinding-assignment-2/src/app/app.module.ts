d91032b05b525acea427107375c272aaff1d519d
Kevin Blicharski <kevin-blicharski@uiowa.edu>
2017-12-23 14:59:37 -0600

    (Databinding) Finish my assignment implementation, couldn't fix some issues.

--- assignments/cmp-databinding-assignment-2/src/app/app.module.ts
+++ assignments/cmp-databinding-assignment-2/src/app/app.module.ts
@@ -4,10 +4,16 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { GameControlComponent } from './game-control/game-control.component';
+import { OddComponent } from './odd/odd.component';
+import { EvenComponent } from './even/even.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GameControlComponent,
+    OddComponent,
+    EvenComponent
   ],
   imports: [
     BrowserModule,
@@ -17,4 +23,5 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ assignments/cmp-databinding-assignment-2/src/app/game-control/game-control.component.ts
@@ -0,0 +1,26 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+import { setInterval } from 'timers';
+import Timer = NodeJS.Timer;
+
+@Component({
+  selector: 'app-game-control',
+  templateUrl: './game-control.component.html',
+  styleUrls: ['./game-control.component.css']
+})
+export class GameControlComponent {
+  private ref: Timer;
+  private num = 0;
+  @Output() increment: EventEmitter<number> = new EventEmitter<number>();
+
+  onStopGame() {
+    clearInterval(this.ref);
+  }
+
+  onStartGame() {
+    this.ref = setInterval(() => {
+      this.increment.emit(this.num);
+      this.num++;
+    }, 1000);
+  }
+
+}
