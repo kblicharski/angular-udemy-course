e5a442136f07764177a0a4fc9ef160ea43907f04
Kevin Blicharski <kevin-blicharski@uiowa.edu>
2017-12-27 20:48:48 -0600

    (Assignments) Add services starter code.

---
+++ assignments/services-assignment-3/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { BrowserModule } from '@angular/platform-browser';
+import { ActiveUsersComponent } from './active-users/active-users.component';
+
+import { AppComponent } from './app.component';
+import { InactiveUsersComponent } from './inactive-users/inactive-users.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ActiveUsersComponent,
+    InactiveUsersComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
